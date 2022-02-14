ff88b210a3a5612d47fcd018c02df30b1a894d4f
sam tiso <samtiso@Sammys-computer.local>
2022-02-14 17:08:42 -0500

    pushing again for troubleshoot

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         {/* links to route path */}
         <Link to = '/names'>SHOW CANDIDATES</Link>
         <Link to = '/'>HOME</Link>
+        <a href='https://www.fec.gov/data/candidates/?has_raised_funds=true&is_active_candidate=true'>Get FEC ID Number</a>
       </nav>
       <Routes>
         <Route path ='/' element={<Home />} />
