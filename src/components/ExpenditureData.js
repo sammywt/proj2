4f719eccf123876005ccef48e852ef4022d8ab72
sam tiso <samtiso@Sammys-computer.local>
2022-02-16 16:48:40 -0500

    toggle function set up and working but expands all elements rather than just the selected one

--- src/components/ExpenditureData.js
+++ src/components/ExpenditureData.js
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom"
 import ExpenditureDetails from "./ExpenditureInfo";
+import { useState } from "react";
 
 
 const ExpenditureData = ({expenditures}) => {
+    const [isActive, setActive] = useState(false);
+    const ToggleClass = () => {
+        setActive(!isActive);
+    }
+
     return (
         expenditures.map((i, index) => (
             <ul key={index}>
                 <li>PURPOSE: {i.purpose}</li>
-                <p>details</p>
-                <div className='toggle'>
+                <p onClick={ToggleClass}>details</p>
+                <div className={isActive ? 'active' : 'notActive'}>
                     <li>AMOUNT: {i.amount}</li>
                     <li>PURPOSE: {i.purpose}</li>
                     <li>RECIPIENT: {i.payee}</li>
